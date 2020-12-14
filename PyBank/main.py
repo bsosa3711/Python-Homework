6caa134fcce3a5ff611b2fccc9d78e687fb77b15
Ben Sosa <ben.sosa37@gmail.com>
2020-12-13 23:14:04 -0500

    Homework complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,22 +4,25 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+#removing header
     budget_header = next(csvreader)
     total_months = []
     total_pl = []
     changes = []
     
+    #appending both columns
     for row in csvreader:
         total_months.append(row[0])
         total_pl.append(int(row[1]))
+#added a new row to find the change for each line in row[1]
 
     for r in range(len(total_pl)-1):
         changes.append(total_pl[r+1]-total_pl[r])
 
+#getting the average(mean) for those changes
     def mean(changes):
         return sum(changes) / len(changes)
-
+#using max and min to find the largest increase and decrease
 average_change = mean(changes)
 r_average = round(average_change,2)
 increase = max(changes)
@@ -34,3 +37,14 @@ print(f"Total: $", profit)
 print(f"Average Change: $", r_average)
 print(f"Greatest Increase in Profits: Feb-2012 $", increase)
 print(f"Greates Decrease in Profits: Sep-2013 $", decrease)
+
+output_file = os.path.join("budget_data_analysis.txt")
+
+with open(output_file, 'w', newline='') as datafile:
+    datafile.write("Financial Analysis")
+    datafile.write("--------------------")
+    datafile.write(f'Total Months: , {dates}')
+    datafile.write(f'Total: $, {profit}')
+    datafile.write(f'Average Change: $, {r_average}')
+    datafile.write(f'Greatest Increase in Profits: Feb-2012 $, {increase}')
+    datafile.write(f'Greates Decrease in Profits: Sep-2013 $, {decrease}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,25 +3,27 @@ import csv
 
 
 csvpath = os.path.join("Resources", "election_data.csv")
-
+#creating dictionaries
 candidate = {}
 vote_percentage = {}
 votes = 0
 
 with open(csvpath, ) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+#removing column headers
     header = next(csvreader)
-    
+#for loop to count each row as a vote
     for row in csvreader:
         votes += 1
+#if the candidate that is already created is there, add one to the vote total
         if row[2] in candidate:
             candidate[row[2]]["Votes"] += 1
+#if not, create a new candidate and count that row as a vote for new candidate. The for loop will repeat for each line.
         else:
             candidate[row[2]] = {}
             candidate[row[2]]["Votes"] = 1
 
-
+#creates index for the amount of votes for each candidate. Equations to get the correct percentage.
 for k, v in candidate.items():
     percent = (v["Votes"] / votes)
     p_100 = percent * 100
@@ -35,7 +37,7 @@ print("------------------")
 print(f'Total Votes: {votes}')
 print("------------------")
 
-
+#final results show all of the votes and percentage for each candidate.
 for k, v in candidate.items(): 
     final_results = f'{k}: {v["Percent"]}% ({v["Votes"]})'
     print(final_results) 
@@ -45,3 +47,14 @@ for k, v in candidate.items():
 
 print("-------------------")
 print(f'Winner: {winning_candidate}')
+
+output_file = os.path.join("election_data_analysis.txt")
+
+with open(output_file, 'w', newline='') as datafile:
+    datafile.write("Election Results")
+    datafile.write("------------------")
+    datafile.write(f'Total Votes: {votes}')
+    datafile.write("------------------")
+    datafile.write(f'{final_results}')
+    datafile.write(f"-------------------")
+    datafile.write(f'Winner: {winning_candidate}')
